867023bc70fd29e396c515390b3d0d35c5a6d0ce
renjithapraveen <63199113+renjithapraveen@users.noreply.github.com>
2026-01-07 10:53:05 +0530

    Create calculate.java

---
+++ calculate.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Choose operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result = " + (a + b));
+                break;
+            case 2:
+                System.out.println("Result = " + (a - b));
+                break;
+            case 3:
+                System.out.println("Result = " + (a * b));
+                break;
+            case 4:
+                if (b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Division by zero is not allowed");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
